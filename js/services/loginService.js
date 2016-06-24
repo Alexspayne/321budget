d06f44c14b86cda04138e22a6042f457cf96146e
Alex Payne <alexpaynex@gmail.com>
2016-06-24 08:50:58 -0400

    Making progress towards migrating session handling

---
+++ js/services/loginService.js
@@ -0,0 +1,13 @@
+angular
+    .module("ngBudget")
+    .service('loginService', function($http) {
+	$http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+	this.login = function(login){
+	    var req = {
+		method: 'POST',
+		url: '../../php/loginscript.php',
+		data: "login=" + login 		
+	    };
+	    return $http(req);
+	};
+    });
